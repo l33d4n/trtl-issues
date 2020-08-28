0c837df6d9ac0c3a40cda56a167372d29d05bccb
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2020-08-28 02:14:17 +0100

    Speed up loading with parallel api requests (#1)
    * Speed up loading with parallel api requests
    
    * Display github error
    
    * Fix svg not rendering properly
    
    * Support loading from github or local file

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,136 +1,3 @@
-class FetchData {
-
-	static fetchJson(path) {
-		return new Promise((resolve, reject) => {
-			fetch(path)
-				.then((result) => result.json())
-				.then((result) => {
-					resolve(result)
-				})
-				.catch((error) => reject(error))
-		})
-	}
-
-	static fetctGithubApi(url) {
-		return new Promise((resolve, reject) => {
-			fetch(url).then((result) => result.json())
-				.then((result) => {
-					resolve(result)
-				})
-				.catch((error) => reject(error))
-		})
-	}
-
-}
-
-class Tamplates {
-
-	static sideBarItem(repo) {
-		return `<li class="toc-entry toc-h2"><a href="#${repo.repo_name}">${repo.repo_name}</a></li>`
-	}
-
-	static labelItem(label) {
-		return `    
-        <div class="item-labels d-inline-block p-0">
-          <span class="badge" style="background-color: #${label.color};">${label.name}</span>
-       </div>`
-	}
-
-	static issueItem(issue) {
-		let css = issue.comments > 0 ? 'list-item-actions show' : 'd-none'
-
-		return `
-            <div class="list-item">
-            <div class="item">
-            <a href="${issue.html_url}" target="_blank" class="text-body h4 d-inline-block mb-0" data-toggle="tooltip" data-placement="top" title="${issue.body}">${issue.title}</a>
-                ${issue.labels.map((label) => {
-            return Tamplates.labelItem(label)
-        }).join('')}
-                <small class="d-block text-muted mt-n1">#${issue.number} opened on ${fixDate(issue.created_at)} by ${issue.user.login}</small>
-            </div>
-               <a href="${issue.html_url}" target="_blank" class="${css}">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z"></path>
-                        <path d="M4 21v-13a3 3 0 0 1 3 -3h10a3 3 0 0 1 3 3v6a3 3 0 0 1 -3 3h-9l-4 4"></path>
-                        <line x1="8" y1="9" x2="16" y2="9"></line>
-                        <line x1="8" y1="13" x2="14" y2="13"></line>
-                </svg>${issue.comments}
-            </a>
-        </div>
-        `
-	}
-
-	static repoHeader(repo, issuesLength) {
-		return `
-            <div class="card-header">
-               <h3 class="card-title">
-                ${repo.user_name}/${repo.repo_name} 
-                <span class="badge badge-pill bg-green">${issuesLength}</span>
-               </h3>
-           </div>`
-	}
-
-	static dispalyCardRepo(repo, filtredPullRequests) {
-		return `<div id="${repo.repo_name}" class="card">
-                  ${Tamplates.repoHeader(repo, filtredPullRequests.length)}
-                <div class="list list-row list-hoverable">
-                    ${filtredPullRequests.map((issue) => {
-            return Tamplates.issueItem(issue)
-        }).join('')}
-        </div>
-        </div>`
-	}
-
-}
-
-async function main() {
-	spinner(true)
-
-	let cards = ''
-	let totalIsuuse = 0
-
-	const main = document.querySelector('#main')
-	const sideBarList = document.querySelector('.list-unstyled')
-	const bodySideBar = document.querySelector('.sticky-top')
-	bodySideBar.style.display = 'none'
-
-	try {
-		const repos = await FetchData.fetchJson('./repos.json')
-
-		for (const repo of repos) {
-
-			const issuesOfRepo = await FetchData.fetctGithubApi(`https://api.github.com/repos/${repo.user_name}/${repo.repo_name}/issues`)
-
-			if (issuesOfRepo.length > 0) {
-				let filtredPullRequests = filterPullRequest(issuesOfRepo)
-
-				if (filtredPullRequests.length > 0) {
-					totalIsuuse += filtredPullRequests.length
-
-					sideBarList.innerHTML += Tamplates.sideBarItem(repo)
-
-					cards += Tamplates.dispalyCardRepo(repo, filtredPullRequests)
-				}
-			}
-
-			main.innerHTML = cards
-
-			document.querySelector('.page-title').innerText = totalIsuuse + ' Open issuses'
-		}
-
-		bodySideBar.style.display = 'block'
-		spinner(false)
-		$('[data-toggle="tooltip"]').tooltip();
-	} catch (error) {
-		spinner(false)
-		alert(error)
-	}
-}
-
-function filterPullRequest(issuse) {
-	return issuse.filter((issue) => !issue.pull_request)
-}
-
 function fixDate(date) {
 	return date.slice(0, date.indexOf('T'))
 }
@@ -147,4 +14,218 @@ function spinner(status) {
 	}
 }
 
-main()
+function createRepoHeaderHTML(repo, numIssues) {
+    const repoHeader = document.createElement('div');
+    repoHeader.className = 'card-header';
+
+    const header = document.createElement('h3');
+    header.className = 'card-title';
+
+    const headerText = document.createTextNode(repo.full_name);
+
+    const span = document.createElement('span');
+    span.className = 'badge badge-pill bg-green'
+    span.innerText = numIssues;
+
+    header.appendChild(headerText);
+    header.appendChild(span);
+
+    repoHeader.appendChild(header);
+
+    return repoHeader;
+}
+
+function createCommentSVG() {
+    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    const path1 = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    const path2 = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    const line1 = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+    const line2 = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+
+    svg.setAttribute('class', 'icon');
+    svg.setAttribute('width', '24');
+    svg.setAttribute('height', '24');
+    svg.setAttribute('viewBox', '0 0 24 24');
+    svg.setAttribute('stroke-width', '2');
+    svg.setAttribute('stroke', 'currentColor');
+    svg.setAttribute('fill', 'none');
+    svg.setAttribute('stroke-linecap', 'round');
+    svg.setAttribute('stroke-linejoin', 'round');
+
+    path1.setAttribute('d', 'M0 0h24v24H0z');
+    path1.setAttribute('stroke', 'none');
+    path2.setAttribute('d', 'M4 21v-13a3 3 0 0 1 3 -3h10a3 3 0 0 1 3 3v6a3 3 0 0 1 -3 3h-9l-4 4');
+
+    line1.setAttribute('x1', '8');
+    line1.setAttribute('x2', '16');
+    line1.setAttribute('y1', '9');
+    line1.setAttribute('y2', '9');
+
+    line2.setAttribute('x1', '8');
+    line2.setAttribute('x2', '14');
+    line2.setAttribute('y1', '13');
+    line2.setAttribute('y2', '13');
+
+    svg.appendChild(path1);
+    svg.appendChild(path2);
+    svg.appendChild(line1);
+    svg.appendChild(line2);
+
+    return svg;
+}
+
+async function fetchRepoIssues(repo) {
+    const issueRes = await fetch(`https://api.github.com/repos/turtlecoin/${repo.name}/issues`, {
+        headers: {
+            Authorization: '',
+        },
+    });
+
+    let issues = await issueRes.json();
+
+    issues = issues.filter((i) => !i.pull_request);
+
+    if (issues.length === 0) {
+        return;
+    }
+
+    const repoDiv = document.createElement('div');
+    repoDiv.id = repo.name;
+    repoDiv.className = 'card';
+    repoDiv.appendChild(createRepoHeaderHTML(repo, issues.length));
+
+    const issuesDiv = document.createElement('div');
+    issuesDiv.className = 'list list-row list-hoverable';
+
+    for (const issue of issues) {
+        const listItemDiv = document.createElement('div');
+        listItemDiv.className = 'list-item';
+        issuesDiv.appendChild(listItemDiv)
+
+        const itemDiv = document.createElement('div');
+        itemDiv.className = 'item';
+
+        const issueLink = document.createElement('a');
+        issueLink.href = issue.html_url;
+        issueLink.target = '_blank';
+        issueLink.className = 'text-body h4 d-inline-block mb-0';
+        issueLink.dataset.toggle = 'tooltip';
+        issueLink.dataset.placement = 'top';
+        issueLink.title = issue.body;
+        issueLink.textContent = issue.title;
+        itemDiv.appendChild(issueLink);
+
+        for (const label of issue.labels) {
+            const labelDiv = document.createElement('div');
+            labelDiv.className = 'item-labels d-inline-block p-0';
+
+            const span = document.createElement('span');
+            span.className = 'badge';
+            span.style = `background-color: #${label.color};`;
+            span.textContent = label.name;
+
+            labelDiv.appendChild(span);
+
+            itemDiv.appendChild(labelDiv);
+        }
+
+        /* Probably done better in CSS... */
+        const small = document.createElement('small');
+        small.className = 'd-block text-muted mt-n1';
+        small.textContent = `${issue.number} opened on ${fixDate(issue.created_at)} by ${issue.user.login}`;
+        itemDiv.appendChild(small);
+
+        listItemDiv.appendChild(itemDiv);
+
+        const issueCommentsLink = document.createElement('a');
+        issueCommentsLink.href = issue.html_url;
+        issueCommentsLink.target = '_blank';
+        issueCommentsLink.className = issue.comments > 0
+            ? 'list-item-actions show'
+            : 'd-none';
+
+        issueCommentsLink.appendChild(createCommentSVG());
+        issueCommentsLink.appendChild(document.createTextNode(issue.comments));
+
+        listItemDiv.appendChild(issueCommentsLink);
+        issuesDiv.appendChild(listItemDiv);
+    }
+
+    repoDiv.appendChild(issuesDiv);
+
+    return {
+        repoIssues: repoDiv,
+        count: issues.length,
+    };
+}
+
+async function loadGithubIssues() {
+    const useLocalIssues = true;
+
+    const issuesURL = useLocalIssues
+        ? './repos.json'
+        : 'https://api.github.com/orgs/turtlecoin/repos?sort=updated';
+
+    try {
+        const res = await fetch(issuesURL, {
+            headers: {
+                Authorization: '',
+            },
+        });
+
+        const repos = await res.json();
+
+        /* GitHub error */
+        if (repos.message) {
+            alert(repos.message);
+            return;
+        }
+
+        const mainDiv = document.getElementById('main');
+        const sidebar = document.querySelector('.list-unstyled');
+        const bodySideBar = document.querySelector('.sticky-top');
+        const title = document.querySelector('.page-title');
+
+        bodySideBar.style.display = 'none';
+
+        let issueCount = 0;
+
+        const promises = [];
+
+        for (const repo of repos) {
+            const issues = fetchRepoIssues(repo);
+            promises.push(issues);
+
+            issues.then(({ repoIssues, count } = {}) => {
+                if (repoIssues) {
+                    mainDiv.appendChild(repoIssues);
+
+                    const sidebarItem = document.createElement('li');
+                    sidebarItem.className = 'toc-entry toc-h2';
+
+                    const repoLink = document.createElement('a');
+                    repoLink.href = `#${repo.name}`;
+                    repoLink.textContent = repo.name;
+
+                    sidebarItem.appendChild(repoLink);
+
+                    sidebar.appendChild(sidebarItem);
+
+                    issueCount += count;
+
+                    title.innerText = `${issueCount} Open Issues`;
+                }
+            });
+        }
+
+        /* Wait for all promises to complete */
+        await Promise.all(promises);
+
+        bodySideBar.style.display = 'block';
+		$('[data-toggle="tooltip"]').tooltip();
+    } catch (err) {
+        alert(`Failed to fetch data: ${err.toString()}`);
+    }
+}
+
+window.addEventListener('load', loadGithubIssues);
